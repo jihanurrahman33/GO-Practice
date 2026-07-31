4ad0fecc74bab9db7caac371a6ce62c8d540414a
jihanurrahman33 <jihanurrahman33@gmail.com>
2026-07-31 23:04:52 +0600

    Refactor goroutine calls in main function for improved clarity and timing

--- concurrency/go_routine/main.go
+++ concurrency/go_routine/main.go
@@ -15,10 +15,13 @@ func main() {
 
 	fmt.Println("Hello from Main() Goroutine")
 
-	go sayHello("Hello World!", time.Second)
+	go sayHello("Hello World! 1", time.Second)
+	go sayHello("Hello World! 2", time.Second)
+	go sayHello("Hello World! from 2 seconds", 2*time.Second)
+	go sayHello("Hello World! from 3 seconds", 3*time.Second)
 
 	fmt.Println("Last message from main() goroutine")
 
-	time.Sleep(2 * time.Second)
+	time.Sleep(3 * time.Second)
 
 }

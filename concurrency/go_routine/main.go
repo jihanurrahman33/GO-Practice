24f62d932439592453a90198d1c36289a0d96bba
jihanurrahman33 <jihanurrahman33@gmail.com>
2026-08-01 22:27:21 +0600

    Add timing output for sayHello2 function and improve clarity in main

--- concurrency/go_routine/main.go
+++ concurrency/go_routine/main.go
@@ -12,6 +12,11 @@ func sayHello(message string, delay time.Duration, wg *sync.WaitGroup) {
 	fmt.Println("sayHello", message)
 }
 
+func sayHello2(message string, delay time.Duration) {
+	time.Sleep(delay)
+	fmt.Println("sayHello", message)
+}
+
 func main() {
 
 	var wg sync.WaitGroup
@@ -27,5 +32,16 @@ func main() {
 
 	wg.Wait()
 	fmt.Println("Last message from main() goroutine")
-	fmt.Println("Total time taken:", time.Since(startTime))
+	fmt.Println("Total time taken by sayHello():", time.Since(startTime))
+
+	startTime = time.Now()
+
+	sayHello2("Hello World! 1", time.Second)
+	sayHello2("Hello World! 2", time.Second)
+	sayHello2("Hello World! from 2 seconds", 2*time.Second)
+	sayHello2("Hello World! from 3 seconds", 3*time.Second)
+	sayHello2("Hello World! from 4 seconds", 3*time.Second)
+	sayHello2("Hello World! from 5 seconds", 3*time.Second)
+
+	fmt.Println("Total time taken by sayHello2():", time.Since(startTime))
 }

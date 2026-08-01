1ba216cb780cf941ca863d02acccb9fa72019a67
jihanurrahman33 <jihanurrahman33@gmail.com>
2026-08-01 22:24:43 +0600

    Refactor sayHello function to include WaitGroup for better concurrency management

--- concurrency/go_routine/main.go
+++ concurrency/go_routine/main.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func sayHello(message string, delay time.Duration) {
-
+func sayHello(message string, delay time.Duration, wg *sync.WaitGroup) {
+	defer wg.Done()
 	time.Sleep(delay)
 	fmt.Println("sayHello", message)
 }
 
 func main() {
 
+	var wg sync.WaitGroup
+	startTime := time.Now()
 	fmt.Println("Hello from Main() Goroutine")
+	wg.Add(6)
+	go sayHello("Hello World! 1", time.Second, &wg)
+	go sayHello("Hello World! 2", time.Second, &wg)
+	go sayHello("Hello World! from 2 seconds", 2*time.Second, &wg)
+	go sayHello("Hello World! from 3 seconds", 3*time.Second, &wg)
+	go sayHello("Hello World! from 4 seconds", 3*time.Second, &wg)
+	go sayHello("Hello World! from 5 seconds", 3*time.Second, &wg)
 
-	go sayHello("Hello World! 1", time.Second)
-	go sayHello("Hello World! 2", time.Second)
-	go sayHello("Hello World! from 2 seconds", 2*time.Second)
-	go sayHello("Hello World! from 3 seconds", 3*time.Second)
-
+	wg.Wait()
 	fmt.Println("Last message from main() goroutine")
-
-	time.Sleep(3 * time.Second)
-
+	fmt.Println("Total time taken:", time.Since(startTime))
 }
